ad4497359dc9672b749e45301887a4650711806e
harold Guerrero <69978749+hguerrer@users.noreply.github.com>
2021-07-10 12:47:36 -0400

    Update varyParam.py

--- varyParam.py
+++ varyParam.py
@@ -6,9 +6,10 @@ class parameters:
 
         def __init__(self,name):
                 self.name=name
-
+                self.value=[v for v in float(sys.argv[2])]
         def Print(self):
-                process.g4SimHits.MagneticField.ConfGlobalMFM.OCMS.StepperParam.self.name = cms.double(float(sys.argv[2]))
+                process.g4SimHits.MagneticField.ConfGlobalMFM.OCMS.StepperParam.self.name = cms.double(self.value[0])
 
 p1=parameters('EnergyThSimple')
 parameters.Print(p1)
+
