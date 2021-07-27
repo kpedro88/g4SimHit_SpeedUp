0d447924db93ba38ff1d3c38dd5342d9ec7be12b
harold Guerrero <69978749+hguerrer@users.noreply.github.com>
2021-07-27 11:11:33 -0400

    Update Script_RunGeantSettings.sh

--- Final_Result/Script_RunGeantSettings.sh
+++ Final_Result/Script_RunGeantSettings.sh
@@ -1,1 +1,12 @@
+#!/bin/bash
 
+source Parameters_two.sh $varnames
+echo ${my_array[@]}
+
+for VAL in "${my_array[@]}"; do
+        LOG=log_${varnames}_${VAL/0./}.txt
+        ROOT=PPD-RunIISummer20UL17Sim-${VAL/0./}.root
+        echo $varnames $VAL
+        cmsRun PPD_RunIISummer20UL17SIM_0_cfg.py paramNames=$varnames paramValues=$VAL >& $LOG
+        echo $VAL $(grep "Total loop" $LOG | tail -n 1 | rev | cut -d' ' -f1 | rev)
+done
