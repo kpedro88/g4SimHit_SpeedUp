068c3a22709404c33ef55b2fda56b5efb0376aa1
harold Guerrero <69978749+hguerrer@users.noreply.github.com>
2021-07-27 11:21:14 -0400

    Automatically created by SCRAM

---
+++ Final_Result/python/__init__.py
@@ -0,0 +1,3 @@
+#Automatically created by SCRAM
+import os
+__path__.append(os.path.dirname(os.path.abspath(__file__).rsplit('/g4SimHit_SpeedUp/Final_Result/',1)[0])+'/cfipython/slc7_amd64_gcc820/g4SimHit_SpeedUp/Final_Result')
