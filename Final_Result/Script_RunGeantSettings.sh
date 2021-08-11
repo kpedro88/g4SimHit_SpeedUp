415d17be1754970d020c03270fb24661704dc5b4
harold Guerrero <69978749+hguerrer@users.noreply.github.com>
2021-08-11 16:44:31 -0400

    Update Script_RunGeantSettings.sh

--- Final_Result/Script_RunGeantSettings.sh
+++ Final_Result/Script_RunGeantSettings.sh
@@ -3,13 +3,16 @@
 source Parameters.sh
 source Parameters_two.sh $varnames
 
+PRU=changes_${par}.txt
+PRU1=changes_${varnames}.txt
+
 if [ $# -eq 0 ]; then
         for par in "${!parameters[@]}"; do
                 for VAL in "${!parameters[$par]}"; do
                         LOG=log_${par}_${VAL}.txt
                         echo $par $VAL
                         cmsRun PPD_RunIISummer20UL17SIM_0_cfg.py paramNames=$par paramValues=$VAL >& $LOG
-                        echo $VAL $(grep "Total loop" $LOG | tail -n 1 | rev | cut -d' ' -f1 | rev)
+                        echo $VAL $(grep "Total loop" $LOG | tail -n 1 | rev | cut -d' ' -f1 | rev) | tee -a $PRU
                 done
         done
 else
@@ -17,7 +20,7 @@ else
                 LOG=log_${varnames}_${VAL}.txt
                 echo $varnames $VAL
                 cmsRun PPD_RunIISummer20UL17SIM_0_cfg.py paramNames=$varnames paramValues=$VAL >& $LOG
-                echo $VAL $(grep "Total loop" $LOG | tail -n 1 | rev | cut -d' ' -f1 | rev)
+                echo $VAL $(grep "Total loop" $LOG | tail -n 1 | rev | cut -d' ' -f1 | rev) | tee -a $PRU1
         done
-
 fi
+
