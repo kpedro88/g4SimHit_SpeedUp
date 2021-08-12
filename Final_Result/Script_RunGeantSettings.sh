d9f1a056a1588adc5e5f287eb554b67306ce5f16
harold Guerrero <69978749+hguerrer@users.noreply.github.com>
2021-08-12 16:11:30 -0400

    Update Script_RunGeantSettings.sh

--- Final_Result/Script_RunGeantSettings.sh
+++ Final_Result/Script_RunGeantSettings.sh
@@ -7,14 +7,12 @@
 source Parameters.sh
 source Parameters_two.sh $varnames
 
-PRU=changes_${par}.txt
-PRU1=changes_${varnames}.txt
-
 #the if statement allows the user to establish whether he wants to work with all the parameters or only with one of them
 if [ $# -eq 0 ]; then
         for par in "${!parameters[@]}"; do                 # parameter loop
                 for VAL in "${!parameters[$par]}"; do      # current parameter's values loop
                         LOG=log_${par}_${VAL}.txt          # initialize log for the current parameter and value 
+                        PRU=changes_${par}.txt
                         echo $par $VAL
                         cmsRun PPD_RunIISummer20UL17SIM_0_cfg.py paramNames=$par paramValues=$VAL >& $LOG             # logs the cmsRun, parsing the current paramter and value
                         echo $VAL $(grep "Total loop" $LOG | tail -n 1 | rev | cut -d' ' -f1 | rev) | tee -a $PRU     # prints the current value and Total loop time of the cmsRun and save the results in a .txt file
@@ -23,6 +21,7 @@ if [ $# -eq 0 ]; then
 else
         for VAL in "${!parameters[$varnames]}"; do         # current parameter's values loop
                 LOG=log_${varnames}_${VAL}.txt             # initialize log for the current parameter and value
+                PRU1=changes_${varnames}.txt
                 echo $varnames $VAL
                 cmsRun PPD_RunIISummer20UL17SIM_0_cfg.py paramNames=$varnames paramValues=$VAL >& $LOG               # logs the cmsRun, parsing the current paramter and value
                 echo $VAL $(grep "Total loop" $LOG | tail -n 1 | rev | cut -d' ' -f1 | rev) | tee -a $PRU1           # prints the current value and Total loop time of the cmsRun and save the results in a .txt file
