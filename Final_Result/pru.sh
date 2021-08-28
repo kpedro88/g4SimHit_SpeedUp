9e6da6794c31a175c9d9fac63b4994df44178d59
harold Guerrero <69978749+hguerrer@users.noreply.github.com>
2021-08-28 14:05:58 -0400

    combining parameters

--- Final_Result/pru.sh
+++ Final_Result/pru.sh
@@ -6,7 +6,7 @@ for VAL in "${!parameters[${args[0]}]}"; do
                 LOG=log_${args[0]}_and_${args[1]}_${VAL}_${val}.txt
                 PRU1=changes_${args[0]}_and_${args[1]}.txt
                 echo ${args[0]} ${args[1]} $VAL $val
-                cmsRun PPD_RunIISummer20UL17SIM_0_cfg.py paramNames=${args[0]}, ${args[1]} paramValues=$VAL, $val >& $LOG
+                cmsRun PPD_RunIISummer20UL17SIM_0_cfg.py paramNames=${args[0]},${args[1]} paramValues=$VAL,$val >& $LOG
                 echo $VAL $val $(grep "Total loop" $LOG | tail -n 1 | rev | cut -d' ' -f1 | rev) | tee -a $PRU1
         done
 done
