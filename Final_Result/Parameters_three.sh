98b83b8243248eaaec65f472b0fcc825d871fcbc
harold Guerrero <69978749+hguerrer@users.noreply.github.com>
2021-08-30 11:11:00 -0400

    combining parameters

--- Final_Result/Parameters_three.sh
+++ Final_Result/Parameters_three.sh
@@ -14,4 +14,19 @@ args=("$@")
 if [ "${args[0]}" = "RusRoGammaEnergyLimit" ] && [ "${args[1]}" = "RusRoEcalGamma" ]; then
         parameters[RusRoGammaEnergyLimit]=RusRo_energy[@]
         parameters[RusRoEcalGamma]=RusRo_vals[@]
+elif [ "${args[0]}" = "RusRoGammaEnergyLimit" ] && [ "${args[1]}" = "RusRoHcalGamma" ]; then
+        parameters[RusRoGammaEnergyLimit]=RusRo_energy[@]
+        parameters[RusRoHcalGamma]=RusRo_vals[@]
+elif [ "${args[0]}" = "RusRoGammaEnergyLimit" ] && [ "${args[1]}" = "RusRoMuonIronGamma" ]; then
+        parameters[RusRoGammaEnergyLimit]=RusRo_energy[@]
+        parameters[RusRoMuonIronGamma]=RusRo_vals[@]
+elif [ "${args[0]}" = "RusRoNeutronEnergyLimit" ] && [ "${args[1]}" = "RusRoEcalNeutron" ]; then
+        parameters[RusRoNeutronEnergyLimit]=RusRo_energy[@]
+        parameters[RusRoEcalNeutron]=RusRo_vals[@]
+elif [ "${args[0]}" = "RusRoNeutronEnergyLimit" ] && [ "${args[1]}" = "RusRoHcalNeutron" ]; then
+        parameters[RusRoNeutronEnergyLimit]=RusRo_energy[@]
+        parameters[RusRoHcalNeutron]=RusRo_vals[@]
+elif [ "${args[0]}" = "RusRoNeutronEnergyLimit" ] && [ "${args[1]}" = "RusRoMuonIronNeutron" ]; then
+        parameters[RusRoNeutronEnergyLimit]=RusRo_energy[@]
+        parameters[RusRoMuonIronNeutron]=RusRo_vals[@]
 fi
