7c3a597fdcfa9b1c75dbf2725fbe5a671d37af14
harold Guerrero <69978749+hguerrer@users.noreply.github.com>
2021-09-15 11:27:47 -0400

    Update graph.py

--- Final_Result/graph.py
+++ Final_Result/graph.py
@@ -1,19 +1,43 @@
 import matplotlib.pyplot as plt
-from numpy import loadtxt
 
 #lists where time and change values are stored
 time = []
 changes = []
 
-#reading the .txt file
-graph = loadtxt("changes_RusRoEcalGamma.txt",float)
-for row in graph:
-    time.append(float(row[0]))
-    changes.append(float(row[1]))
+#reading multiple files through one function
+def files(filename):
+        try:
+                with open(filename) as f_obj:
+                        for line in f_obj:
+                                if line.startswith('TimeReport> Time report complete in'):
+                                        one = line.find('n')
+                                        two = line.find('s')
+                                        a = line[one+2:two-1]
+        except:
+                msg = filename + " file does not exist "
+                print(msg)
+        return time.append(float(a))
+
+#files
+filenames = ['log_RusRoEcalGamma_0.1.txt', 'log_RusRoEcalGamma_0.01.txt', 'log_RusRoEcalGamma_0.001.txt', 'log_RusRoEcalGamma_0.0001.txt', 'log_RusRoEcalGamma_0.00001.txt']
+
+#the for loop executes each of the files in the function
+for filename in filenames:
+        files(filename)
+
+#in this loop the value of the changes are extracted
+for value in filenames:
+        one = value.find('0')
+        two = value.find('t')
+        b = value[one:two-1]
+        changes.append(float(b))
+
+#print(changes)
+#print(time)
 
 #graphing the behavior of the parameter
-plt.plot(time,changes)
+plt.plot(changes,time)
 plt.xlabel('Changes')
-plt.ylabel('Total loop')
+plt.ylabel('Time')
 plt.title('RusRoEcalGamma', size = 15)
 plt.show()
